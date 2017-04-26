ab51d7c0977a1596fdc6402909f564fe68dea32b
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-04-26 14:07:33 +0200

    Added object state publisher and cleaned up the code a bit

--- src/node_manager.py
+++ src/node_manager.py
@@ -5,61 +5,40 @@ import rospy
 import subprocess, shlex
 from std_msgs.msg import Int32
 from kobuki_msgs.msg import Sound
-#from geometry_msgs.msg import PoseWithCovarianceStamped, Quaternion
-#from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
 pub_start = None
 sound_pub = None
+ost_pub = None
 
 def init():
-    global sound_pub, pub_start
+    global sound_pub, pub_start, ost_pub
     rospy.init_node('radio_node_manager')
     rospy.Subscriber('radio_node_manager_main_controller/instruction', Int32, instructionCallback)
     sound_pub = rospy.Publisher('mobile_base/commands/sound', Sound, queue_size=1)
+    ost_pub = rospy.Publisher("motion_analysis/object_state", Int32)
 
     #Run here all the initial nodes
     time.sleep(15)
     initial_pose()
     time.sleep(10)
-    #pub_start = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=10)
-    #start_point = PoseWithCovarianceStamped()
-    #start point position x
-    #start_point.pose.pose.position.x = -6.329
-    #start point position y     
-    #start_point.pose.pose.position.y = 10.087
-    #start_point.header.stamp = rospy.Time.now()
-    #start_point.pose.pose.orientation.z = -2.122
-    #quat = quaternion_from_euler(0.0, 0.0, -2.122) # roll, pitch, yaw
-    #start_point.pose.pose.orientation = Quaternion(*quat.tolist())
-    #start_point.pose.pose.orientation.w = 0
-    #start_point.header.frame_id = 'map'
-    #rospy.sleep(1)
-    #pub_start.publish(start_point)
     sound_msg = Sound()
     sound_msg.value = 6
     sound_pub.publish(sound_msg)
 
     while not rospy.is_shutdown():  
         rospy.spin()
-    #we reach this point only after Ctrl-C
-    #HPR(False)
-    #rosVisual(False)
-    #motionAnalysisHuman(False)
-    #motionAnalysisObject(False)
 
 def instructionCallback(msg):
     if msg.data == 0:
-        HPR(True)
+        HPR()
     elif msg.data == 1:
-        motionAnalysisHuman(True)
+        motionAnalysisHuman()
     elif msg.data == 2:
-        # TODO change configuration
-        motionAnalysisObject(True)
+        motionAnalysisObject(1)
     elif msg.data == 22:
-        # TODO change configuration
-        motionAnalysisObject(True)
+        motionAnalysisObject(2)
     elif msg.data == 3:
-        rosVisual(True)
+        rosVisual()
     elif msg.data == 4:
         command = "roslaunch kobuki_auto_docking minimal.launch"
         command = shlex.split(command)
@@ -73,51 +52,50 @@ def instructionCallback(msg):
     elif msg.data == 5:
         initial_pose()
 
-def HPR(start):
+def HPR():
     global sound_pub
-    if start:
-        print 'Starting HPR'
-        command = "roslaunch human_pattern_recognition hpr.launch"
-        command = shlex.split(command)
-        subprocess.Popen(command)
-        sound_msg = Sound()
-        sound_msg.value = 0
-        sound_pub.publish(sound_msg)
+    print 'Starting HPR'
+    command = "roslaunch human_pattern_recognition hpr.launch"
+    command = shlex.split(command)
+    subprocess.Popen(command)
+    sound_msg = Sound()
+    sound_msg.value = 0
+    sound_pub.publish(sound_msg)
 
-def motionAnalysisHuman(start):
-    global sound_pub
-    if start:
-        print 'Starting motion_analysis human'
-        command = "roslaunch motion_analysis human_event_detection.launch"
-        command = shlex.split(command)
-        subprocess.Popen(command)
-        time.sleep(10)
-        sound_msg = Sound()
-        sound_msg.value = 0
-        sound_pub.publish(sound_msg)
+def motionAnalysisHuman():
+    global sound_pub, ost_pub
+    print 'Starting motion_analysis human'
+    command = "roslaunch motion_analysis human_event_detection.launch"
+    command = shlex.split(command)
+    subprocess.Popen(command)
+    time.sleep(10)
+    ost_pub.publish(2)
+    sound_msg = Sound()
+    sound_msg.value = 0
+    sound_pub.publish(sound_msg)
 
-def motionAnalysisObject(start):
+def motionAnalysisObject(mode):
     global sound_pub
-    if start:
-        print 'Starting motion_analysis object'
-        command = "roslaunch motion_analysis object_event_detection.launch"
-        command = shlex.split(command)
-        subprocess.Popen(command)
-        time.sleep(10)
-        sound_msg = Sound()
-        sound_msg.value = 0
-        sound_pub.publish(sound_msg)
+    print 'Starting motion_analysis object'
+    command = "roslaunch motion_analysis object_event_detection.launch"
+    if mode == 2:
+        command = "roslaunch motion_analysis object_event_detection2.launch"
+    command = shlex.split(command)
+    subprocess.Popen(command)
+    time.sleep(10)
+    sound_msg = Sound()
+    sound_msg.value = 0
+    sound_pub.publish(sound_msg)
 
-def rosVisual(start):
+def rosVisual():
     global sound_pub
-    if start:
-        print 'Starting ros_visual'
-        command = "roslaunch ros_visual ros_visual_classifier.launch"
-        command = shlex.split(command)
-        subprocess.Popen(command)
-        sound_msg = Sound()
-        sound_msg.value = 0
-        sound_pub.publish(sound_msg)
+    print 'Starting ros_visual'
+    command = "roslaunch ros_visual ros_visual_classifier.launch"
+    command = shlex.split(command)
+    subprocess.Popen(command)
+    sound_msg = Sound()
+    sound_msg.value = 0
+    sound_pub.publish(sound_msg)
 
 def initial_pose():
     command = "rosservice call /marker_mapping_node/init_pose_from_marker \"id: []\""
